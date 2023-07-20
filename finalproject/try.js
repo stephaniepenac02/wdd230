47a1990ba8ac79e049040b7303faffa5f3fb7a6b
stephaniepenac02 <stephaniepenac@yahoo.com>
2023-07-19 23:55:26 -0500

    week14pr

--- finalproject/try.js
+++ finalproject/try.js
@@ -1,103 +1,44 @@
-fetch(bountiful.json)
-  .then(function (response) {
-    // The JSON data will arrive here
-  })
-  .catch(function (err) {
-    // If an error occured, you will catch it here
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
   });
 
-
-
-
-
-
-$(document).ready(function() {
-    $.getJSON("bountiful.json", function(data){
-        console.log(data)
-
-        $("fruits").html(data.fruits);
-
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var selectedRow = null;
-function onFormSubmit(e){
-    event.preventDefault();
-    var formData = readFormData();
-    if(selectedRow === null){
-        insertNewRecord(formData);
-    }else{
-        updateRecord(formData)
-    }
-    resetForm();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Read operation using this function
-function readFormData(){
-    var formData = {};
-    formData["fullName"] = document.getElementById("fname").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["number"] = document.getElementById("number").value;
-    formData["fruit"] = document.getElementById("fruit").value;
-    return formData;
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
 }
 
+initMap();
+const position = { lat: -25.344, lng: 131.031 };
+// Request needed libraries.
+//@ts-ignore
+const { Map } = await google.maps.importLibrary("maps");
+const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-
-// To Reset the data of fill input
-function resetForm(){
-    document.getElementById('fname').value = '';
-    document.getElementById('empCode').value = '';
-    document.getElementById('number').value = '';
-    document.getElementById('fruit').value = '';
-    selectedRow = null;
-}
-
-
-function updateRecord(formData){
-    selectedRow.cells[0].innerHTML = formData.fname;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.number;
-    selectedRow.cells[3].innerHTML = formData.fruit;
-}
-function onDelete(td){
-    if(confirm('Are you sure you want to delete this record?')){
-        row = td.parentElement.parentElement;
-        document.getElementById('employeeList').deleteRow(row.rowIndex);
-        resetForm();
-    }
-}
-
-
-
-
+// The map, centered at Uluru
+map = new Map(document.getElementById("map"), {
+  zoom: 4,
+  center: position,
+  mapId: "DEMO_MAP_ID",
+});
+const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
